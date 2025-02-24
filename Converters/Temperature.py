c2b04c80ffdaccc570e27f4435c2350e88a86b17
Juaneg0 <juanegomezgomez@gmail.com>
2025-02-24 14:18:29 +1100

    Temperature Functions

---
+++ Converters/Temperature.py
@@ -0,0 +1,6 @@
+
+def celsius_to_fahrenheit(celsius):
+    return float((float(celsius) * 9.0/5.0) + 32.0)
+
+def fahrenheit_to_celsius(fahrenheit):
+    return float((float(fahrenheit) - 32.0) * 5.0/9.0)
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print ("hello world")
+import Converters.Temperature as convert_Temperature
+
+Celcius = float(input("Enter a temperature in Celcius: "))
+Fahrenheit = convert_Temperature.celsius_to_fahrenheit(Celcius)
+
+print("Temperature in Fahrenheit: ", Fahrenheit)
