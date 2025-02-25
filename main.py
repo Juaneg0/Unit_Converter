afef3db27c004132b7780e24633b0ca673f3e048
Juaneg0 <juanegomezgomez@gmail.com>
2025-02-25 23:16:05 +1100

    3th Commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import Converters.Temperature as convert_Temperature
 
+
+print ("------ Units Converter ------")
 Celcius = float(input("Enter a temperature in Celcius: "))
 Fahrenheit = convert_Temperature.celsius_to_fahrenheit(Celcius)
 
---
+++ menu.py
@@ -0,0 +1,6 @@
+def main_menu():
+    print("1. Convert temperature")
+    print("2. Convert distance")
+    choice = input("Enter your choice: ")
+    return choice
+
