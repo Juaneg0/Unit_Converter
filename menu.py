b8dffdf38e34683a7e5dbf243f3959c6606f67c0
Juaneg0 <juanegomezgomez@gmail.com>
2025-02-26 23:20:25 +1100

    init

--- menu.py
+++ menu.py
@@ -1,6 +1,6 @@
 def main_menu():
     print("1. Convert temperature")
-    print("2. Convert distance")
+    print("2. Convert distance ")
     choice = input("Enter your choice: ")
     return choice
 
