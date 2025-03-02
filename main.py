3423e8fa8316c662a9ed82ece01ef78adba5f562
Juaneg0 <juanegomezgomez@gmail.com>
2025-03-02 18:24:19 +1100

    Basic_Menu_implementation

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import Converters.Temperature as convert_Temperature
+import menu
 
-
-print ("------ Units Converter ------")
-Celcius = float(input("Enter a temperature in Celcius: "))
-Fahrenheit = convert_Temperature.celsius_to_fahrenheit(Celcius)
-
-print("Temperature in Fahrenheit: ", Fahrenheit)
+choice = 1
+while choice != 0:
+    choice = menu.main_menu()
+    if choice == 1:
+        choice = menu.temperature_menu()
+        if choice == 1:
+            Celcius = float(input("Enter a temperature in Celcius: "))
+            Fahrenheit = convert_Temperature.celsius_to_fahrenheit(Celcius)
+            print("Temperature in Fahrenheit: ", Fahrenheit)
+        elif choice == 2:
+            Fahrenheit = float(input("Enter a temperature in Fahrenheit: "))
+            Celcius = convert_Temperature.fahrenheit_to_celsius(Fahrenheit)
+            print("Temperature in Celcius: ", Celcius)
--- menu.py
+++ menu.py
@@ -1,6 +1,14 @@
 def main_menu():
+    print ("------ Units Converter ------")
     print("1. Convert temperature")
     print("2. Convert distance ")
+    print("0. EXIT")
     choice = input("Enter your choice: ")
-    return choice
+    return int(choice)
 
+def temperature_menu():
+    print("------ Temperature Conversion ------")
+    print("1. Celcius to Fahrenheit")
+    print("2. Fahrenheit to Celcius")
+    choice = input("Enter your choice: ")
+    return int(choice)
