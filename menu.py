f248a5017701bdfbf9c9f1ff072ef9f8fd6762d5
Juaneg0 <juanegomezgomez@gmail.com>
2025-04-27 21:58:31 +1000

    Celcius to fahrenheit menu working

--- menu.py
+++ menu.py
@@ -1,5 +1,11 @@
 from tkinter import *
 from tkinter import ttk
+import Converters.Temperature as convert_Temperature
+import Converters.Distance as convert_Distance
+import Converters.Weight as convert_Weight
+
+def get_value_from_entry(entry):
+    return entry.get()
 
 def clean_screen(root):
     for widget in root.winfo_children():
@@ -27,9 +33,27 @@ def temperature_menu(root):
     clean_screen(root)
     root_label_title = Label(root, text = "Temperature Converter", bg = "light blue", font = ("Arial", 20)).grid(row = 0, column = 0, padx=100)
     root_label_text = Label(root, text = "Choose a conversion", bg = "light blue", font = ("Arial", 15)).grid(row = 1, column = 0, padx=100)
-    root_temp_button = Button(root, text = "Celcius to Fahrenheit").grid(row = 2, column = 0, padx=100)
+    root_temp_button = Button(root, text = "Celcius to Fahrenheit", command = lambda:celcius_to_fahrenheit_menu(root)).grid(row = 2, column = 0, padx=100)
     root_distance_button = Button(root, text = "Fahrenheit to Celcius").grid(row = 3, column = 0, padx=100)
     root_quit = Button(root, text = "Go back", command = lambda:main_menu(root)).grid(row = 4, column = 0)
+
+def print_converted_value(entry):
+        fahrenheit = 0.0
+        celsius = float(entry.get())
+        fahrenheit = convert_Temperature.celsius_to_fahrenheit(celsius)
+        root_label_text = Label(root, text = "")
+        root_label_text.grid(row = 2, column = 1, padx=0)
+        root_label_text.config(text = str(fahrenheit) + "°F", bg = "light blue", font = ("Arial", 15)).grid(row = 2, column = 1, padx=0)
+
+def celcius_to_fahrenheit_menu(root):
+    clean_screen(root)
+    fahrenheit = 0.0
+    root_label_title = Label(root, text = "Celcius to Fahrenheit Converter", bg = "light blue", font = ("Arial", 20)).grid(row = 0, column = 1, padx=0)
+    root_label_text = Label(root, text = "Enter the temperature in Celcius", bg = "light blue", font = ("Arial", 15)).grid(row = 1, column = 0, padx=0)
+    root_temp_entry = Entry(root)
+    root_temp_entry.grid(row = 1, column = 1, padx=0)
+    root_temp_button = Button(root, text = "Convert", command = lambda:print_converted_value(root_temp_entry)).grid(row = 1, column = 2, padx=0)
+    root_quit = Button(root, text = "Go back", command = lambda:temperature_menu(root)).grid(row = 3, column = 1, padx=0)
 
 def distance_menu(root):
     clean_screen(root)
